c4be0a85df4b9489ef56650a4d32b46090759b6e
Stellarr2 <pauljoel11perez@gmail.com>
2025-09-28 22:51:36 +0800

    Delete Project Files directory

--- Project Files/capstone_utils/status_rules.py
+++
@@ -1,8 +0,0 @@
-status_rules = {
-    "shipped": {"confirmed": True, "delivery": "In Transit", "billing": "Processed"},
-    "disputed": {"confirmed": True, "delivery": "Not in Transit", "billing": "Not Processed"},
-    "in process": {"confirmed": True, "delivery": "Not in Transit", "billing": "Not Processed"},
-    "on hold": {"confirmed": False, "delivery": "Not in Transit", "billing": "Not Processed"},
-    "resolved": {"confirmed": True, "delivery": "Delivered", "billing": "Processed"},
-    "cancelled": {"confirmed": True, "delivery": "Not in Transit", "billing": "Not Processed"},
-}
